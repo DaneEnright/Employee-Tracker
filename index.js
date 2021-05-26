915aecd20603dd77c4c00030a1b6f504c9f27dbc
Dane Enright <enrightco@comcast.net>
2021-05-25 23:21:15 -0600

    created read functions for employee, departments, roles

--- index.js
+++ index.js
@@ -6,51 +6,127 @@
 
 const inquirer = require("inquirer");
 const { allowedNodeEnvironmentFlags } = require("process");
-require("console.table");
+const { connection } = require("./db/index");
+const myslq = require("mysql");
+const table = require("console.table");
 const dbindex = require("./db/index");
 
-const start = () => {
-    inquirer
-     .prompt([
-         {
-         name: "start",
-         type: "list",
-         message: "Which option would you like to choose",
-         choices: [
-             "View All Employees",
-             "View Departments",
-             "View Roles",
-             "Add New Employee",
-             "Add New Department",
-             "Add new Role",
-             "Update Roles",
-         ],
-         },
-     ])
-     .then ((answer) => {
-         switch (answer.start) {
-             case "View All Employees":
-                 return viewEmployees();
-                 break;
-            case "View Departments":
-                return viewDepartments();
-                break;
-            case "View Roles":
-                return viewRoles();
-                break;
-            case "Add New Employee":
-                return addEmployee();
-                break;
-            case "Add New Department":
-                return addDepartment();
-                break;
-            case "Add New Role":
-                return addRole();
-                break;
-            case "Update Roles":
-                return updateRole();
-                break;
-            
-         }
-     })
-}
+const startApp = () => {
+  inquirer
+    .prompt([
+      {
+        name: "start",
+        type: "list",
+        message: "Which option would you like to choose",
+        choices: [
+          "View All Employees",
+          "View Departments",
+          "View Roles",
+          "Add New Employee",
+          "Add New Department",
+          "Add new Role",
+          "Update Roles",
+        ],
+      },
+    ])
+    .then((answer) => {
+      switch (answer.start) {
+        case "View All Employees":
+          viewEmployees();
+          break;
+        case "View Departments":
+          viewDepartments();
+          break;
+        case "View Roles":
+          viewRoles();
+          break;
+        case "Add New Employee":
+          createEmployee();
+          break;
+        case "Add New Department":
+          createDepartment();
+          break;
+        case "Add New Role":
+          createRole();
+          break;
+        case "Update Roles":
+          updateRole();
+          break;
+      }
+    });
+};
+
+const viewEmployees = () => {
+  connection.query("SELECT * FROM employee", (err, data) => {
+    if (err) throw err;
+    console.table(data);
+    startApp();
+  });
+};
+
+const viewDepartments = () => {
+  connection.query("SELECT * FROM department", (err, data) => {
+    if (err) throw err;
+    console.table(data);
+    startApp();
+  });
+};
+
+const viewRoles = () => {
+  connection.query("SELECT * FROM roles", (err, data) => {
+    if (err) throw err;
+    console.table(data);
+    startApp();
+  });
+};
+
+const createEmployee = () => {
+  connection.query(
+    "INSERT INTO employee SET ?",
+    {
+      first_name: "Dane",
+      last_name: "Enright",
+      role_id: "10",
+      manager_id: "null",
+    },
+    (err, res) => {
+    //   if (err) throw err;
+      connection.end;
+    }
+  );
+};
+
+const createDepartment = () => {
+  connection.query(
+    "INSERT INTO department SET ?",
+    {
+      department_name: "Security",
+    },
+    (err, res) => {
+      if (err) throw err;
+      connection.end;
+    }
+  );
+};
+
+const createRole = () => {
+  connection.query(
+    "INSERT INTO roles SET ?",
+    {
+      title: "Engineer",
+    },
+    (err, res) => {
+      if (err) throw err;
+      connection.end;
+    }
+  );
+};
+
+const updateRole = () => {
+    const query = connection.query(
+        "UPDATE roles SET ? WHERE ?"
+        
+    )
+};
+
+startApp();
