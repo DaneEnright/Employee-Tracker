e3357379116f6918c86c44e10b43101ea2fed688
Dane Enright <enrightco@comcast.net>
2021-05-25 21:32:23 -0600

    added start function for inquirer questions

--- db/schema.sql
+++ db/schema.sql
@@ -2,12 +2,13 @@ DROP DATABASE IF EXISTS employees_db;
 
 CREATE DATABASE employees_db;
 
-USE DATABASE employees_db;
+USE employees_db;
 
 CREATE TABLE department (
 
   id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  department_name  VARCHAR(30) 
+  department_name  VARCHAR(30),
+  department_id INTEGER 
 );
 
 CREATE TABLE roles (
@@ -26,3 +27,32 @@ CREATE TABLE employee (
   role_id INTEGER,
   manager_id INTEGER
 );
+
+INSERT INTO department (department_name, department_id) values ("Sales", 1);
+INSERT INTO department (department_name, department_id) values ("Engineering", 2);
+INSERT INTO department (department_name, department_id) values ("Finance", 3);
+INSERT INTO department (department_name, department_id) values ("Legal", 4);
+
+INSERT INTO roles (title, salary, department_id) values ("Sales Lead", 100000, 1);
+INSERT INTO roles (title, salary, department_id) values ("Sales Person", 80000, 1);
+INSERT INTO roles (title, salary, department_id) values ("Lead Engineer", 150000, 2);
+INSERT INTO roles (title, salary, department_id) values ("Software Engineer", 120000, 2);
+INSERT INTO roles (title, salary, department_id) values ("Secretary", 60000, 3);
+INSERT INTO roles (title, salary, department_id) values ("Accountant", 125000, 3);
+INSERT INTO roles (title, salary, department_id) values ("Legal Team Lead", 250000, 4);
+INSERT INTO roles (title, salary, department_id) values ("Lawyer", 190000, 4);
+INSERT INTO roles (title, salary, department_id) values ("Lead Engineer", 150000, 2);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) values ("John", "Doe", 1, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) values ("Mike", "Chan", 2, null) ;
+INSERT INTO employee (first_name, last_name, role_id, manager_id) values ("Ashley", "Rodriguez", 3, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) values ("Kevin", "Tupic", 4, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) values ("Jane", "Doe", 5, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) values ("Malia", "Brown", 6, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) values ("Sarah", "Lourd", 7, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) values ("Tom", "Allen", 8, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) values ("Christian", "Eckenrode", 9, null);
+
+SELECT * FROM department;
+SELECT * FROM roles;
+SELECT * FROM employee;
--- index.js
+++ index.js
@@ -3,3 +3,54 @@
 //inqurier for questions
 // switch case for action functions
 // functions that are connected to switch case
+
+const inquirer = require("inquirer");
+const { allowedNodeEnvironmentFlags } = require("process");
+require("console.table");
+const dbindex = require("./db/index");
+
+const start = () => {
+    inquirer
+     .prompt([
+         {
+         name: "start",
+         type: "list",
+         message: "Which option would you like to choose",
+         choices: [
+             "View All Employees",
+             "View Departments",
+             "View Roles",
+             "Add New Employee",
+             "Add New Department",
+             "Add new Role",
+             "Update Roles",
+         ],
+         },
+     ])
+     .then ((answer) => {
+         switch (answer.start) {
+             case "View All Employees":
+                 return viewEmployees();
+                 break;
+            case "View Departments":
+                return viewDepartments();
+                break;
+            case "View Roles":
+                return viewRoles();
+                break;
+            case "Add New Employee":
+                return addEmployee();
+                break;
+            case "Add New Department":
+                return addDepartment();
+                break;
+            case "Add New Role":
+                return addRole();
+                break;
+            case "Update Roles":
+                return updateRole();
+                break;
+            
+         }
+     })
+}
